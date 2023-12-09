20de5da3f8efdc5b24bc10e4780acd7db8120668
VIGNESH VASU <vigneshvasu0511@gmail.com>
2023-12-09 15:39:19 +0530

    update

--- Day1/src/Components/NavbarReg.jsx
+++ Day1/src/Components/NavbarReg.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import LogoImage from 'C:\Users\vicky\OneDrive\Documents\GitHub\React_IRC\Day1\public\V logo.jpg';
 
 function Navbar() {
     return (
         <div class="navb">
   <nav class="navbar">
+    
     <div>
-    <img src="/V logo.jpg" alt="Logo" className="logo-image" />
+    <img src={LogoImage} alt="Logo" className="logo-image" />
     </div>
+    
     <div class="links">
       <a href="/Login" class="href1">Login</a>
       <a href="#">Register</a>
