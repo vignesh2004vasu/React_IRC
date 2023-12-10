8ce54434ca132fa06ddf3963b5b118f6a407242d
VIGNESH VASU <vigneshvasu0511@gmail.com>
2023-12-10 18:06:59 +0530

    update to adduser and show added user

--- Day2/sampleapp/src/Components/Register.jsx
+++ Day2/sampleapp/src/Components/Register.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import '../Assests/css/Register.css'
 import Navbar from './Navbar';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Register(){
+
+    let navigate=useNavigate();
 
     const [user,setUser]=useState({
         username:"",
@@ -12,6 +16,19 @@ function Register(){
 
     const{username,email,password}=user
 
+    const onInputChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
+
+    const onSubmit=async (e)=>{
+        e.preventDefault();
+
+        await axios.post("http://localhost:8080/user",user)
+        navigate("/");
+
+
+    };
+    
   return (
     <div>
       <Navbar/>
@@ -19,19 +36,19 @@ function Register(){
      
       <div className="login-container">
         <h2>Login</h2>
-        <form className="login-form">
+        <form className="login-form" onSubmit={(e)=>onSubmit(e)}>
           <div className="input-group">
 
-            <input type="text" id="username" name="username" placeholder="Enter Username" value={username} />
+            <input type={"text"} id="username" name="username" placeholder="Enter Username" value={username} onChange={(e)=>onInputChange(e)} />
           </div>
           <div className="input-group">
-            <input type="password" id="password" name="password" placeholder="Enter Username" value={email}/>
+            <input type={"email"} id="email" name="email" placeholder="Enter Email" value={email} onChange={(e)=>onInputChange(e)}/>
           </div>
           <div className="input-group">
-                     <input type="password" id="password" name="password" placeholder="Enter Password" value={password}/>
+                     <input type={"password"} id="password" name="password" placeholder="Enter Password" value={password} onChange={(e)=>onInputChange(e)}/>
           </div>
           <div className="input-group">
-                     <input type="password" id="cpassword" name="password" placeholder="Confirm Password"/>
+                     <input type={"password"} id="cpassword" name="password" placeholder="Confirm Password"/>
           </div>
           <button type="submit">Submit</button>
         </form>
