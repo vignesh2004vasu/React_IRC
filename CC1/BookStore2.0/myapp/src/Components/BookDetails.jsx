9e0d317c6765439c7262aa0ac7263b43c765ea25
VIGNESH VASU <vigneshvasu0511@gmail.com>
2023-12-16 15:35:00 +0530

    update

--- CC1/BookStore2.0/myapp/src/Components/BookDetails.jsx
+++ CC1/BookStore2.0/myapp/src/Components/BookDetails.jsx
@@ -1,17 +1,14 @@
 import { useLocation } from "react-router-dom";
 
 function BookDetails(props) {
-// Access the passed book details
-
-  // Use the book details object to display specific information
 
   return (
     <div>
-      {/* Display book details */}
+      
       <h2>Book Details</h2>
       <p>Title: {props.title}</p>
       <p>Author: {props.author}</p>
-      {/* Display other book details using the retrieved data */}
+      
     </div>
   );
 }
